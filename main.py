11ad5a1971b451bab89dd6e8500f103f2156c249
Harshita Singh <harshita.singh@ABZ-556.abzooba.taas.com>
2023-08-02 16:07:55 +0530

    Added the main.py file for the project

---
+++ main.py
@@ -0,0 +1,7 @@
+
+"""
+This is the main practice python file for our project.
+This is where we will be trying out all the git commands that we learn along the development
+of this demo project
+"""
+print("Hello git")
